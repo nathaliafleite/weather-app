564c8661fa655993ebdd905d57460aaf03081713
nathaliafleite <nathalia.leite2@gmail.com>
2023-08-02 14:58:13 +0100

    first commit

---
+++ weather/test/nathaliafleite/weather_test.cljs
@@ -0,0 +1,10 @@
+(ns nathaliafleite.weather-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [nathaliafleite.weather :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
